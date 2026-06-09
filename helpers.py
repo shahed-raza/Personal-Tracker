8dcb43bad222240fe17eca8a561c4e0ffe51f299
Humza <humza0406@gmail.com>
2026-06-09 17:26:30 +0000

    Add helpers.py, random quote generation

---
+++ helpers.py
@@ -0,0 +1,6 @@
+import random
+
+
+def get_quote():
+    quotes = ["Be Fucking Bored!", "Remember, You certainly wouldn't like to do stuff, at a point of time, but keep going, don't stop even if you want to, atleast do the bare minimum", "Aim for progress, no matter how minute it is, not perfection", "Perfection is never attained"]
+    return random.choice(quotes)
