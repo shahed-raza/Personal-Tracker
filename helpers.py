a9bbc1fa926db82a48d95c2d5831acc3152889d1
Humza <humza0406@gmail.com>
2026-06-10 16:29:49 +0000

    Fix quotes formatting

--- helpers.py
+++ helpers.py
@@ -2,5 +2,10 @@ import random
 
 
 def get_quote():
-    quotes = ["Be Fucking Bored!", "Remember, You certainly wouldn't like to do stuff, at a point of time, but keep going, don't stop even if you want to, atleast do the bare minimum", "Aim for progress, no matter how minute it is, not perfection", "Perfection is never attained"]
+    quotes = [
+        "Be Fucking Bored!",
+        "Remember, You certainly wouldn't like to do stuff, at a point of time, but keep going, don't stop even if you want to, atleast do the bare minimum",
+        "Aim for progress, no matter how minute it is, not perfection",
+        "Perfection is never attained"
+    ]
     return random.choice(quotes)
