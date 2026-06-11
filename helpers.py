da7ebc01a4f23adbe479cdb7049c424bfc7d1eea
Humza <humza0406@gmail.com>
2026-06-11 18:51:15 +0000

    Copy pasted login_required(), apology()

--- helpers.py
+++ helpers.py
@@ -1,4 +1,45 @@
+from flask import session, redirect, render_template
+from functools import wraps
 import random
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def apology(message, code=400):
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
 def get_quote():
