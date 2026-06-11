ffc104ab6a914eb738df50c4a1a1cc5f869c535a
Humza <humza0406@gmail.com>
2026-06-11 19:22:52 +0000

    Ensure respones aren't cached
    - render apology page in-case of errors
    - decided to calculate time-left on client-side using js
    
    Todo:
    - decided to make app for multiple users
    - so, appropriate user register, login, logout
    - logic and templates are to written
    - time-left calculation to be done using js

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from datetime import time
-from flask import Flask, render_template, request, flash, redirect
-from flask_session import Session
 import sqlite3
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import get_quote
+from helpers import get_quote, login_required, apology
 
 app = Flask(__name__)
 
@@ -16,6 +16,15 @@ Session(app)
 dbname = "tracker.db"
 cx = sqlite3.connect(dbname)
 cursor = cx.cursor()
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route("/")
@@ -36,21 +45,23 @@ def log():
         problems = request.form.get("problems")
 
         if title is None or title == "":
-            redirect("/log") # js created error element mentioning error
+            return apology("Title of the task is empty or None")
 
         if time_taken is None or time_taken == "":
-            redirect("/log") # js created error element mentioning error
+            return apology(f"Time taken to do {title} is empty or None")
+
         try:
             time_taken = int(time_taken)
             if time_taken < 1 or 12 < time_taken:
-                redirect("/log") # js created error element mentioning error
+                return apology("Time taken field is not in between 1 and 12")
         except ValueError:
-            redirect("/log") # js created error element mentioning error
+            return apology("Time taken field is not an integer")
+
         if distractions and distractions == "":
-            redirect("/log") # js error mentioning, provide non-empty distractions
+            return apology("Distractions fields is empty")
 
         if problems and problems == "":
-            redirect("/log") # provide non-empty problems
+            return apology("Problems fields is empty")
 
         cursor.execute("""
                        INSERT INTO "logs"
@@ -78,34 +89,28 @@ def todo():
     #   time_left conversions, bound checks
     # time-left calculation and reduction if successful to-do task give
     if request.method == "POST":
-        time_left = request.form.get("time-left")
+        # TODO:
+        # time-left should be made via javascript, at the client-machine
+        # time-left should be taken as input, and the appropriate time which left
+        # should be displayed via dom
+        # it also, needs to be reduced for succesive todo taks added by the duration of time each task consumes
+
         title = request.form.get("title")
         eta_time_taken = request.form.get("estimated-time-taken")
 
         if title is None or title == "":
-            redirect("/todo") # js error element
+            return apology("Todo task's title emtpy or None")
+
         if eta_time_taken is None or eta_time_taken == "":
-            redirect("/todo") # error element
-        if time_left is None or time_left == "":
-            redirect("/todo") # error element
-
-        try:
-            time_left = int(time_left)
-        except ValueError:
-            redirect("/todo") # error message
-
-        if time_left < 1 or 12 < time_left:
-            redirect("/todo") # error message
+            return redirect(f"Estimated time taken for {title} is empty or None")
 
         try:
             eta_time_taken = int(eta_time_taken)
         except ValueError:
-            redirect("/todo") # error message
+            return apology(f"Estimated time taken for {title} is not an integer")
 
         if eta_time_taken < 1 or 12 < eta_time_taken:
-            redirect("/todo") # error message
-
-        time_left -= eta_time_taken
+            return apology(f"Estimated time taken for {title} is not in between 1 and 12")
 
         cursor.execute("""
                        INSERT INTO "todos"
@@ -115,8 +120,5 @@ def todo():
                        (title, eta_time_taken, 0)
         )
         cx.commit()
-        time_left_dict = {"hours": time_left, "minutes": 0}
-        return render_template("todo.html", time_left=time_left_dict)
-
 
     return render_template("todo.html")
